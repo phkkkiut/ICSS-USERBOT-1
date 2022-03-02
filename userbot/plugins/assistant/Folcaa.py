a470f2647aa82879d00d55ba453db776e5e98410
phkkkiut <100495583+phkkkiut@users.noreply.github.com>
2022-03-03 01:50:23 +0200

    Update and rename kimo.py to Folcaa.py

---
+++ userbot/plugins/assistant/Folcaa.py
@@ -0,0 +1,8 @@
+#  PeLe - USERBOT
+#  TELE - @UU_VOL
+
+from . import Devt
+
+@asst_cmd("المطور")
+async def _(folcaa):
+    await folcaa.reply(Devt)
--- userbot/plugins/assistant/kimo.py
+++
@@ -1,8 +0,0 @@
-#  ICSS - USERBOT
-#  TELE - @NIIIN2
-
-from . import Devt
-
-@asst_cmd("المطور")
-async def _(kimo):
-    await kimo.reply(Devt)
